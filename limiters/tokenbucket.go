1af61c9b86762a63981cf5cf3ebda180baa17290
Peter <peteratkinson1994@gmail.com>
2021-12-21 23:24:16 +0000

    refactor on token bucket limiter

--- limiters/tokenbucket.go
+++ limiters/tokenbucket.go
@@ -26,7 +26,7 @@ func New(client *redis.Client, limitPeriod time.Duration, rate int) *Redis {
 
 func (r *Redis) Update(ctx context.Context, key string) error {
 	now := time.Now()
-	unixNow := now.Unix()
+	timeStamp := now.Unix()
 
 	values, err := r.client.HGetAll(ctx, key).Result()
 	if err != nil {
@@ -36,7 +36,7 @@ func (r *Redis) Update(ctx context.Context, key string) error {
 
 	if len(values) == 0 {
 		// No hash exists at that key, we can create a new one (-1 on rate as first hit is one use of a token)
-		r.client.HSet(ctx, key, "tokens", fmt.Sprint(r.rate-1), "ts", fmt.Sprint(unixNow))
+		take(ctx, r.client, key, r.rate, timeStamp)
 		return nil
 	}
 
@@ -46,28 +46,16 @@ func (r *Redis) Update(ctx context.Context, key string) error {
 		return err
 	}
 
-	// Get tokens from Redis HASH
-	tokens, err := strconv.Atoi(values["tokens"])
-
-	if err != nil {
-		return err
-	}
-
-	// Do a bunch of convertions on the STRING values
-	tsUnix := time.Unix(ts, 0)
-	delta := unixNow
-
-	deadline := tsUnix.Add(r.limitPeriod).Unix()
+	deadline := time.Unix(ts, 0).Add(r.limitPeriod).Unix()
 	// IF the current time distance between now and the last access timestamp is higher than the deadline timestamp
-	if delta >= deadline {
+	if timeStamp >= deadline {
 		// refill the bucket
-		r.client.HSet(ctx, key, "tokens", fmt.Sprint(r.rate), "ts", fmt.Sprint(unixNow))
+		update(ctx, r.client, key, r.rate, timeStamp)
 	} else {
 		// Otherwise check if we have any tokens left in the bucket
-		if tokens > 0 {
+		if tokens, _ := strconv.Atoi(values["tokens"]); tokens > 0 {
 			// If we do then we can update the Hash with the new token count and timestamp
-			remainder := tokens - 1
-			r.client.HSet(ctx, key, "tokens", fmt.Sprint(remainder), "ts", ts)
+			take(ctx, r.client, key, r.rate, timeStamp)
 		} else {
 			// Otherwise, we've exceeded the limit before the deadline, return error
 			return errors.ErrorLimitExceeded(r.rate, time.Unix(deadline, 0))
@@ -75,6 +63,14 @@ func (r *Redis) Update(ctx context.Context, key string) error {
 	}
 
 	return nil
+}
+
+func update(ctx context.Context, client *redis.Client, key string, rate int, ts int64) {
+	client.HSet(ctx, key, "tokens", fmt.Sprint(rate), "ts", fmt.Sprint(ts))
+}
+
+func take(ctx context.Context, client *redis.Client, key string, rate int, ts int64) {
+	update(ctx, client, key, rate-1, ts)
 }
 
 func NewClient(limitPeriod time.Duration, rate int) (Redis, func()) {
